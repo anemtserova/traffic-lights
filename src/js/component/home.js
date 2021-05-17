1f67a842c91f396c2f1c7961a242c1e675308cd3
Antoniya <anemtserova@gmail.com>
2021-05-17 22:40:12 +0000

    set interval to change lights

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
-	const [active, setActive] = useState(null);
+export function Home(props) {
+	//const [active, setActive] = useState(null);
+
 	return (
 		<div className="container d-flex flex-column justify-content-center align-items-center">
 			<div
-				onClick={() => setActive("red")}
+				//onClick={() => setActive("red")}
 				className={` ${
-					active !== "red" ? "light" : "light red"
+					props.active == "red" ? "light red" : "light"
 				}`}></div>
 			<div
-				onClick={() => setActive("yellow")}
+				//onClick={() => setActive("yellow")}
 				className={
-					active !== "yellow" ? "light" : "light yellow"
+					props.active == "yellow" ? "light yellow" : "light"
 				}></div>
 			<div
-				onClick={() => setActive("green")}
-				className={active !== "green" ? "light" : "light green"}></div>
+				//onClick={() => setActive("green")}
+				className={
+					props.active == "green" ? "light green" : "light"
+				}></div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	active: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { useEffect, useState } from "react";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -12,4 +13,12 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let color = "red";
+const changeLights = () => {
+	if (color == "red") color = "yellow";
+	else if (color == "yellow") color = "green";
+	else if (color == "green") color = "red";
+	ReactDOM.render(<Home active={color} />, document.querySelector("#app"));
+};
+
+setInterval(changeLights, 2000);
